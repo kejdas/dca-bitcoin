e6690753eeacb6d9421f1afe6cd14ca45c06f94c
kejdas <64081579+kejdas@users.noreply.github.com>
2025-10-04 17:51:46 +0200

    Update dca.py
    port changes

--- dca.py
+++ dca.py
@@ -222,4 +222,5 @@ def chart():
     return render_template('chart.html', plot_url=plot_url)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
